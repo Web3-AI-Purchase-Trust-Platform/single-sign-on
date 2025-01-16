2938b0b042b845e6668fef339ae6a0c31f237929
MK <qscvdefb@gmail.com>
2025-01-16 21:04:06 +0700

    blabla im mồm nói ít thôi

--- readme.md
+++ readme.md
@@ -1,6 +1,44 @@
-### Version
+# Cách Chạy Ứng Dụng
+
+### Phiên Bản
 - php version: 8.2.26
 - composer version: 2.5.5
 
-### Basic Url:
-- /login
+### Cấu Hình
+- Các thông số trong .template.env
+- **client_id**: client_id trong google oath2
+- **database_host và database_port**: host và port của hệ quản trị cơ sở dữ liệu
+- **jwt_secret**: khóa bí mật để kí và xác thực các token jwt
+
+### Basic Url
+- /login?redirect=...
+- /login/callback?redirect=...&token=...
+- /account-info
+
+<!-- ----------------------------------------------- -->
+# Triển Khai Ứng Dụng
+### Môi Trường Thử Nghiệm
+
+### Môi Trường Thực Tế
+
+<!-- ----------------------------------------------- -->
+# Thông Tin Khác
+
+### Cách Hoạt Động Của Ứng Dụng
+- Tương tác giữa user và hệ thống:
+- Quy trình hoạt động của hệ thống:
+- Các vấn đề về bảo mật:
+
+### Mã Nguồn
+- Cấu trúc:
+- Quản lí mã nguồn:
+
+### Các Thư Viện Phụ Thuộc
+- Các thư viện được quản lí bởi composer - một công cụ quản lý phụ thuộc (dependency manager) cho PHP.
+- **firebase/php-jwt**: Giúp tạo và xác thực chữ ký cho user
+
+### Thông Tin Hệ Thống
+- **Kernel**: `5.15.167.4-microsoft-standard-WSL2`
+- **Hệ điều hành**: **GNU/Linux**
+- **Kiến trúc**: **x86_64**
+- **Nền tảng**: **WSL2**
--- src/login/index.php
+++ src/login/index.php
@@ -13,14 +13,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>SSO - Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Đổi favicon -->
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/128/1791/1791961.png" type="image/png">
 
     <style>
         * {
             margin: 0;
             padding: 0;
+            user-select: none;
         }
     </style>
 </head>
@@ -32,9 +36,9 @@
         align-items: center;
         background-color: #eeeee4;
         background-image: url('https://img.freepik.com/free-photo/christmas-snowy-landscape_1048-3491.jpg?semt=ais_hybrid');
-        background-size: cover; /* Đảm bảo ảnh bao phủ toàn bộ chiều cao và chiều rộng */
-        background-position: center; /* Đảm bảo ảnh được căn giữa */
-        background-repeat: no-repeat; /* Không lặp lại ảnh */
+        background-size: cover; 
+        background-position: center; 
+        background-repeat: no-repeat; 
     ">
 
     <div
@@ -67,7 +71,7 @@
         <!-- Các trường input -->
         <Form
             style = "
-                margin-top: 35px;
+                margin-top: 30px;
                 max-width: 450px;
                 min-width: 300px;
             "
@@ -75,10 +79,10 @@
             action="/login/auth.php"
         >
             <label for="exampleFormControlInput1" class="form-label"><b>Tài khoản</b></label>
-            <input required  name="username" type="text" class="form-control" id="exampleFormControlInput1" placeholder="------">
+            <input required  name="username" type="text" class="form-control" placeholder="------">
 
             <label for="inputPassword5" class="form-label" style="margin-top: 20px;"><b>Mật Khẩu</b></label>
-            <input required  name="password" type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" placeholder="**********">
+            <input required  name="password" type="password" class="form-control" aria-describedby="passwordHelpBlock" placeholder="**********">
         
             <div class="form-check" style="margin-top: 10px;">
                 <input class="form-check-input" type="checkbox" value="1" id="rememberMe" name="rememberMe">
