29b6b2f27f8c9a9c4c2871319b3ba5b185b495f0
MK <qscvdefb@gmail.com>
2025-03-10 19:43:27 +0700

    Update verifyToken.php

--- src/api/verifyToken.php
+++ src/api/verifyToken.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 header('Content-Type: application/json');
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 
